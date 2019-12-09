d1eafaf7e0e3bc51a14ea271ab62533386cf5815
Tarun Verghis <tarun.verghis@airbnb.com>
2019-12-09 10:35:25 -0800

    Update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,4 @@
 abbr -a e nvim
-abbr -a cat bat
 abbr -a grep rg
 abbr -a rgf 'rg --files | rg'
 
@@ -16,26 +15,35 @@ set -g fish_prompt_pwd_dir_length 3
 function fish_prompt
 	set_color brblack
 	echo -n "["(date "+%H:%M")"] "
-	set_color blue
-	echo -n (hostname)
 	if [ $PWD != $HOME ]
-		set_color brblack
-		echo -n ':'
 		set_color yellow
 		echo -n (basename $PWD)
 	end
 	set_color green
-	printf '%s' (__fish_git_prompt)
+
+  set BIG_REPOS 'treehouse'
+  if not contains (basename $PWD) $BIG_REPOS
+    printf '%s' (__fish_git_prompt)
+  end
 	set_color red
 	echo -n '| '
 	set_color normal
 end
+
+bass source ~/.airlab/shellhelper.sh
 
 # Airbnb-specific config
 set -x ENV development
 set -x K2 yes
 set -x ONETOUCHGEN_ACCEPT_EULA yep
 set -x DATA_DIR $HOME/airlab/repos/data
-set -x AFDEV_HOST "i-0108c4fc978d096fa.inst.aws.airbnb.com"
+set -x AFDEV_HOST "i-03923bd837dbdc0f6.inst.aws.airbnb.com"
 set -x AFDEV_PORT 50557
 set -x AFDEV_USER "$USER"
+
+# Java
+set -x JAVA_HOME (/usr/libexec/java_home -v 1.8)
+
+# PATH settings
+set PATH $HOME/.cargo/bin $PATH
+set PATH $JAVA_HOME/bin $PATH
