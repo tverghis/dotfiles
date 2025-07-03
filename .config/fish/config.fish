48e4f07eaed49f67f6d754d0128b0a9d5085574d
Tarun Verghis <tarun.verghis@gmail.com>
2025-07-03 12:52:45 -0700

    Update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,9 @@
-# General aliases
-abbr -a e nvim
+set -U fish_greeting "" # Disable the welcome message
+
+# General Aliases
+abbr -a e helix
 abbr -a cat bat
 abbr -a grep rg
-abbr -a rgf 'rg --files | rg'
 abbr -a ls eza
 
 # Git-specific aliases
@@ -11,28 +12,25 @@ abbr -a gss 'git status -s'
 abbr -a gprom 'git pull --rebase origin main'
 abbr -a gp 'git push -u origin (git branch --show-current)'
 
-set -x EDITOR nvim
+set -x EDITOR helix
 
-set __fish_git_prompt_showuntrackedfiles 'yes'
-set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showuntrackedfiles yes
+set __fish_git_prompt_showdirtystate yes
 set __fish_git_prompt_showstashstate ''
-set __fish_git_prompt_showupstream 'none'
+set __fish_git_prompt_showupstream none
+
 set -g fish_prompt_pwd_dir_length 3
 
 function fish_prompt
-	set_color brblack
-	echo -n "["(date "+%H:%M")"] "
-	set_color blue
-	echo -n (hostname)
-	if [ $PWD != $HOME ]
-		set_color brblack
-		echo -n ':'
-		set_color yellow
-		echo -n (basename $PWD)
-	end
-	set_color green
-	printf '%s' (__fish_git_prompt)
-	set_color red
-	echo -n '| '
-	set_color normal
+    set_color white
+    echo -n "["(date "+%H:%M")"] "
+    if [ $PWD != $HOME ]
+        set_color yellow
+        echo -n (basename $PWD)
+    end
+    set_color green
+    printf '%s' (__fish_git_prompt)
+    set_color red
+    echo -n '| '
+    set_color normal
 end
